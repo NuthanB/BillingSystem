a2abba8b762b860c5ab86d876ec84d8ab0c45c2e
Pavan Bhat <pavanksp612@gmail.com>
2024-03-24 08:45:35 +0530

    removed migrate because no more needed

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.helpers import is_user_logged_in
 
-from flask_migrate import Migrate
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 app.secret_key = '12345'
-migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
