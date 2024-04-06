6c44eec17818571736de43e0f8ea35d12ce49e4a
Pavan Bhat <pavanksp612@gmail.com>
2024-04-06 13:42:37 +0530

    code refactoring

--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ from sqlalchemy.sql import func
 from flask import render_template, request, redirect, session, url_for, jsonify
 from app import app, db
 from app.models import User, Item, Bill, BillItem, UserActivity
-from datetime import datetime
 from sqlalchemy import func
 from collections import defaultdict
 
@@ -14,11 +13,11 @@ def index():
         uid = session['user_id']
         items = Item.query.all()
     except KeyError:
-        return render_template('login.html')
+        return redirect('/login')
+    
     return render_template('index.html', items=items)
 
-
-@app.route('/get_suggestions')
+@app.route('/get-suggestions')
 def get_suggestions():
     keyword = request.args.get('keyword')
     if keyword:
@@ -31,92 +30,62 @@ def get_suggestions():
     else:
         return jsonify([])
 
+@app.route('/get-emails-unames', methods=['GET'])
+def get_emails_unames():
+    emails = [user.email for user in User.query.all()]
+    unames = [user.username for user in User.query.all()]
+    return jsonify({'emails': emails, 'unames': unames})
 
-@app.route('/create_bill', methods=['POST'])
-def create_bill():
-    bill_number = request.form['bill_number']
-    user_id = session.get('user_id')
+@app.route('/get-obj-codes', methods=['GET'])
+def get_obj_codes():
+    codes = [item.code for item in Item.query.all()]
+    return jsonify({'codes': codes})
 
-    if user_id:
-        user = User.query.get(user_id)
-        bill = Bill(bill_number=bill_number, user=user)
-        db.session.add(bill)
-        db.session.commit()
+@app.route("/get-item-name")
+def get_item_name():
+    code = request.args.get('code')
+    try:
+        item_name = Item.query.filter_by(code=code).first().name
+        if item_name:
+            return jsonify({'name': item_name}), 200
+    except:
+        return jsonify({'error': 'Item not found'}), 404
 
-        item_name = request.form['item_name']
-        quantity = int(request.form['quantity'])
-        price = float(request.form['price'])
-        bill_item = BillItem(item_name=item_name,
-                             quantity=quantity, price=price, bill=bill)
-        db.session.add(bill_item)
-        db.session.commit()
+@app.route("/get-item-code")
+def get_item_code():
+    name = request.args.get('name')
+    try:
+        item_code = Item.query.filter_by(name=name).first().code
+        if item_code:
+            return jsonify({'code': item_code}), 200
+    except:
+        return jsonify({'error': 'Item not found'}), 404
 
-        return redirect(url_for('index'))
+@app.route('/get-item-price')
+def get_item_price():
+    code = request.args.get('item_code')
+    item = Item.query.filter_by(code=code).first()
+    if item:
+        return jsonify({'price': item.price})
     else:
-        return redirect(url_for('login'))
+        return jsonify({'error': 'Item not found'})
 
-
-@app.route('/submit_bill', methods=['POST'])
-def submit_bill():
-    if 'user_id' in session:
-        user_id = session['user_id']
-        data = request.json
-
-        if data:
-            items = data.get('items')
-            total = data.get('total')
-            new_bill = Bill(user_id=user_id, total=total)
-            db.session.add(new_bill)
-
-            for item in items:
-                item_db = Item.query.filter_by(
-                    name=item['name']).first()
-
-                if item_db:
-                    new_quantity = item_db.quantity - item['quantity']
-                    print(new_quantity)
-                    if new_quantity >= 0:
-                        item_db.quantity = new_quantity
-                        bill_item = BillItem(
-                            bill=new_bill, item_id=item_db.id, item_name=item['name'], quantity=item['quantity'], price=item['price'])
-                        db.session.add(bill_item)
-                    else:
-                        print("Hi")
-                        db.session.rollback()
-                        return jsonify({'error': f'Insufficient quantity available for {item["name"]}'}), 500
-                else:
-                    db.session.rollback()
-                    return jsonify({'error': f'Item {item["name"]} not found'}), 500
-
-            db.session.commit()
-
-            return jsonify({'message': 'Bill submitted successfully'}), 200
-        else:
-            return jsonify({'error': 'No data received'}), 400
-    else:
-        return jsonify({'error': 'User not authenticated'}), 401
-
-
-def get_bill(bill_id):
-    bill = Bill.query.get(bill_id)
+def get_bill(bill):
     if bill:
         bill_details = {
             'id': bill.id,
             'bill_date_time': bill.bill_date_time.strftime('%d-%m-%Y | %H:%M'),
             'total': bill.total,
-            'bill_items': []  # Initialize an empty list for storing bill items
+            'bill_items': []
         }
 
-        # Query related BillItems
-        bill_items = BillItem.query.filter_by(bill_id=bill_id).all()
+        bill_items = BillItem.query.filter_by(bill_id=bill.id).all()
 
-        # Populate the 'items' list in bill_details with data from BillItems
         for item in bill_items:
             bill_details['bill_items'].append({
                 'item_name': item.item_name,
                 'quantity': item.quantity,
                 'price': item.price
-                # Add more item details here as needed
             })
 
         return bill_details
@@ -124,6 +93,30 @@ def get_bill(bill_id):
     else:
         return jsonify({'error': 'Bill not found'}), 404
 
+
+@app.route('/get-bill-details')
+def get_bill_details():
+    bill_id = request.args.get('bill_id')
+    bill = Bill.query.get(bill_id)
+    if bill:
+        bill_details = get_bill(bill)
+        return jsonify(bill_details)
+    else:
+        return jsonify({'error': 'Bill not found'}), 404
+
+
+
+@app.route('/check-match')
+def check_match():
+    item1Name = request.args.get('item1')
+    item2Code = request.args.get('item2')
+
+    try:
+        item2Name = Item.query.filter_by(code=item2Code).first()
+        if item1Name.lower() == item2Name.lower():
+            return jsonify({'match': 'Y'})
+    except:
+        return jsonify({'error': 'Item not found'})
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -145,7 +138,6 @@ def login():
         else:
             return render_template('login.html', error='Invalid email or password.')
     return render_template('login.html')
-
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -172,11 +164,77 @@ def signup():
 
     return render_template('signup.html', modal_message=modal_message)
 
-@app.route('/get-emails-unames', methods=['GET'])
-def get_emails_unames():
-    emails = [user.email for user in User.query.all()]
-    unames = [user.username for user in User.query.all()]
-    return jsonify({'emails': emails, 'unames': unames})
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/create-bill', methods=['POST'])
+def create_bill():
+    bill_number = request.form['bill_number']
+    user_id = session.get('user_id')
+
+    if user_id:
+        user = User.query.get(user_id)
+        bill = Bill(bill_number=bill_number, user=user)
+        db.session.add(bill)
+        db.session.commit()
+
+        item_name = request.form['item_name']
+        quantity = int(request.form['quantity'])
+        price = float(request.form['price'])
+        bill_item = BillItem(item_name=item_name,
+                             quantity=quantity, price=price, bill=bill)
+        db.session.add(bill_item)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/submit-bill', methods=['POST'])
+def submit_bill():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        data = request.json
+
+        if data:
+            items = data.get('items')
+            total = data.get('total')
+            new_bill = Bill(user_id=user_id, total=total)
+            db.session.add(new_bill)
+
+            for item in items:
+                item_db = Item.query.filter_by(
+                    name=item['name']).first()
+
+                if item_db:
+                    new_quantity = item_db.quantity - item['quantity']
+                    if new_quantity >= 0:
+                        item_db.quantity = new_quantity
+                        bill_item = BillItem(
+                            bill=new_bill, item_id=item_db.id, item_name=item['name'], quantity=item['quantity'], price=item['price'])
+                        db.session.add(bill_item)
+                    else:
+                        db.session.rollback()
+                        return jsonify({'error': f'Insufficient quantity available for {item["name"]}'}), 500
+                else:
+                    db.session.rollback()
+                    return jsonify({'error': f'Item {item["name"]} not found'}), 500
+
+            db.session.commit()
+
+            return jsonify({'message': 'Bill submitted successfully'}), 200
+        else:
+            return jsonify({'error': 'No data received'}), 400
+    else:
+        return jsonify({'error': 'User not authenticated'}), 401
+
+@app.route('/items')
+def items():
+    items = Item.query.all()
+    return render_template('items.html', items=items)
 
 @app.route('/add-item', methods=['GET', 'POST'])
 def add_item():
@@ -202,19 +260,9 @@ def add_item():
             return redirect(url_for('login'))
     return render_template('add_item.html')
 
-@app.route('/get-obj-codes', methods=['GET'])
-def get_obj_codes():
-    codes = [item.code for item in Item.query.all()]
-    return jsonify({'codes': codes})
-
-@app.route('/items')
-def items():
-    items = Item.query.all()
-    return render_template('items.html', items=items)
-
 
 @app.route('/update-stock', methods=['GET', 'POST'])
-def update_item_stock():
+def update_stock():
     if request.method == 'GET':
         items = Item.query.all()
         return render_template('stock_update.html', items=items)
@@ -232,27 +280,8 @@ def update_item_stock():
         return redirect(url_for('items'))
 
 
-@app.route("/get_item_name/<code>")
-def get_item_name(code):
-    try:
-        item_name = Item.query.filter_by(code=code).first().name
-        if item_name:
-            return jsonify({'name': item_name}), 200
-    except:
-        return jsonify({'error': 'Item not found'}), 404
 
-
-@app.route("/get_item_code/<name>")
-def get_item_code(name):
-    try:
-        item_code = Item.query.filter_by(name=name).first().code
-        if item_code:
-            return jsonify({'code': item_code}), 200
-    except:
-        return jsonify({'error': 'Item not found'}), 404
-
-
-@app.route('/edit_item/<int:item_id>')
+@app.route('/edit-item/<int:item_id>')
 def edit_item(item_id):
     item = Item.query.get(item_id)
     if item:
@@ -261,7 +290,7 @@ def edit_item(item_id):
         return redirect(url_for('items'))
 
 
-@app.route('/update_item', methods=['POST'])
+@app.route('/update-item', methods=['POST'])
 def update_item():
     item_id = request.form['id']
     name = request.form['name']
@@ -279,29 +308,11 @@ def update_item():
         db.session.commit()
     return redirect(url_for('items'))
 
-
-@app.route('/get_item_price')
-def get_item_price():
-    code = request.args.get('item_code')
-    item = Item.query.filter_by(code=code).first()
-    if item:
-        return jsonify({'price': item.price})
-    else:
-        return jsonify({'error': 'Item not found'})
-
-
-@app.route('/check_match')
-def check_match():
-    item1Name = request.args.get('item1')
-    item2Code = request.args.get('item2')
-
-    try:
-        item2Name = Item.query.filter_by(code=item2Code).first()
-        if item1Name.lower() == item2Name.lower():
-            return jsonify({'match': 'Y'})
-    except:
-        return jsonify({'error': 'Item not found'})
-
+@app.route('/report')
+def show_reports():
+    bill = Bill.query.all()
+    bill_items = BillItem.query.all()
+    return render_template('report.html', bills=bill, bill_items=bill_items)
 
 @app.route('/filter-bills', methods=['GET', 'POST'])
 def filter_bills():
@@ -309,14 +320,10 @@ def filter_bills():
         from_date = request.form['from_date']
         to_date = request.form['to_date']
 
-        print(from_date, to_date)
-
         filtered_bills = Bill.query.filter(
             func.date(Bill.bill_date_time) >= from_date,
             func.date(Bill.bill_date_time) <= to_date
         ).all()
-
-        print(filtered_bills)
 
         return render_template("filtered_bills.html",
                                bills=filtered_bills,
@@ -325,14 +332,6 @@ def filter_bills():
                                )
     else:
         return redirect("/report")
-
-
-@app.route('/report')
-def show_reports():
-    bill = Bill.query.all()
-    bill_items = BillItem.query.all()
-    return render_template('report.html', bills=bill, bill_items=bill_items)
-
 
 @app.route('/delete-item', methods=['GET', 'POST'])
 def delete_item():
@@ -353,68 +352,32 @@ def delete_item():
         return jsonify({'error': 'Method not allowed'}), 405
 
 
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.clear()  # Clear session data
-    return redirect(url_for('login'))
-
-
-@app.route('/bill-details/<int:bill_id>')
-def get_bill_details(bill_id):
-    bill = Bill.query.get(bill_id)
-    if bill:
-        bill_details = {
-            'id': bill.id,
-            'bill_date_time': bill.bill_date_time.strftime('%d-%m-%Y | %H:%M'),
-            'total': bill.total,
-            'items': []  # Initialize an empty list for storing bill items
-        }
-
-        # Query related BillItems
-        bill_items = BillItem.query.filter_by(bill_id=bill_id).all()
-
-        # Populate the 'items' list in bill_details with data from BillItems
-        for item in bill_items:
-            bill_details['items'].append({
-                'item_name': item.item_name,
-                'quantity': item.quantity,
-                'price': item.price
-                # Add more item details here as needed
-            })
-
-        return jsonify(bill_details)
-    else:
-        return jsonify({'error': 'Bill not found'}), 404
-
-
-@app.route("/print-bill/<int:bill_id>")
-def print_bill(bill_id):
-    print(bill_id)
+@app.route("/print-bill")
+def print_bill():
+    bill_id = int(request.args.get('bill_id'))
+    bill = None
+    
     if bill_id:
-        print("HI")
         try:
             bill = Bill.query.filter_by(id=bill_id).first()
         except Exception as e:
-            print("Error fetching latest bill:", e)
-            return jsonify({'error': 'Failed to fetch latest bill'}), 500
+            print("Error fetching bill:", e)
+            return jsonify({'error': 'Failed to fetch bill'}), 500
     else:
-        print("HELLO")
         try:
             bill = Bill.query.order_by(Bill.id.desc()).first()
+            print(bill)
         except Exception as e:
             print("Error fetching latest bill:", e)
             return jsonify({'error': 'Failed to fetch latest bill'}), 500
 
-    print(bill)
     if bill:
-        bill_id = bill.id
-        bill_details = get_bill(bill_id)
-        print(bill_id, bill_details)
+        bill_details = get_bill(bill)
         [bill_date, bill_time] = bill_details["bill_date_time"].split(
             " | ")
 
         return render_template("token_print.html",
-                               bill_id=bill_id,
+                               bill_id=bill.id,
                                bill_date=bill_date,
                                bill_time=bill_time,
                                total=bill_details["total"],
@@ -424,9 +387,11 @@ def print_bill(bill_id):
         return jsonify({'error': 'No bills found'}), 404
 
 
-@app.route("/print-report/<from_date>/<to_date>")
-def print_report(from_date, to_date):
-    if from_date != 0 and to_date != 0:
+@app.route("/print-report")
+def print_report():
+    from_date = request.args.get('from_date')
+    to_date = request.args.get('to_date')
+    if from_date != '0' and to_date != '0':
         bill = Bill.query.filter(
             func.date(Bill.bill_date_time) >= from_date,
             func.date(Bill.bill_date_time) <= to_date
@@ -439,9 +404,6 @@ def print_report(from_date, to_date):
 
 @app.route("/print-item-report/<from_date>/<to_date>")
 def print_item_report(from_date, to_date):
-    print("Printing...")
-
-    # Query to group items by their group and item name, and get the sum of quantity and total price
     grouped_items = db.session.query(
         Item.group,
         BillItem.item_name,
