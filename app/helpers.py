9a04f76e333b0151957c4e17470ce0e390446091
Amith Kumar <amithkumar8536@gmail.com>
2024-04-19 16:54:14 +0530

    Exe file Created

--- app/helpers.py
+++
@@ -1,4 +0,0 @@
-from flask import session
-
-def is_user_logged_in():
-    return 'user_id' in session
---
+++ run/_internal/static/js/script.js
@@ -0,0 +1,49 @@
+function logout() {
+  // Perform logout actions here, such as redirecting to logout route or clearing session
+  // For example:
+  fetch("/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  })
+    .then((response) => {
+      if (response.ok) {
+        // Handle successful logout
+        console.log("Logged out successfully");
+        // Redirect to login page or perform any other necessary action
+        window.location.href = "/login";
+      } else {
+        // Handle error response
+        console.error("Failed to logout:", response.statusText);
+      }
+    })
+    .catch((error) => {
+      // Handle network error
+      console.error("Error logging out:", error);
+    });
+}
+
+console.log("Hello");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  window.location.reload();
+};
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+const openModal = function (message) {
+  document.getElementById("errmsg").textContent = message;
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  console.log(message);
+};
